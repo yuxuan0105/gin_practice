380f92a45d3bda3e2daa6f53610de502c77d2b47
syuan <sv870105@gmail.com>
2020-03-12 20:44:56 +0800

    getUsers is done

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,6 +1,9 @@
 package v1
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,28 +15,81 @@ type User struct {
 	CreateOn string `json:"create_on"`
 }
 
+func newUser() *User {
+	return &User{}
+}
+
 //ADD: login
 
 func (this *Model) getUsers(c *gin.Context) {
-	//data := interface{}{}
+	data, err := getUserDatas(this.db, "")
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg":  http.StatusText(http.StatusInternalServerError),
+			"data": nil,
+		})
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"msg":  "success",
-		"data": nil,
+		"msg":  http.StatusText(http.StatusOK),
+		"data": data,
 	})
 }
 
 func (this *Model) getUserById(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{
+		"msg":  http.StatusText(http.StatusOK),
+		"data": nil,
+	})
 }
 
 func (this *Model) addUser(c *gin.Context) {
-
+	c.JSON(http.StatusCreated, gin.H{
+		"msg":  http.StatusText(http.StatusCreated),
+		"data": nil,
+	})
 }
 
 func (this *Model) modifyUser(c *gin.Context) {
-
+	c.JSON(http.StatusNoContent, gin.H{
+		"msg":  http.StatusText(http.StatusNoContent),
+		"data": nil,
+	})
 }
 
 func (this *Model) deleteUser(c *gin.Context) {
+	c.JSON(http.StatusNoContent, gin.H{
+		"msg":  http.StatusText(http.StatusNoContent),
+		"data": nil,
+	})
+}
 
+func getUserDatas(db *sql.DB, condition string) ([]*User, error) {
+	query := "SELECT email, nickname,created_on From account"
+	if condition != "" {
+		query += " WHERE " + condition
+	}
+	query += ";"
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("error at querying data: %s", err)
+	}
+	defer rows.Close()
+
+	data := []*User{}
+	for rows.Next() {
+		temp := newUser()
+		if err := rows.Scan(&temp.Email, &temp.Nickname, &temp.CreateOn); err != nil {
+			return nil, fmt.Errorf("error at scaning data: %s", err)
+		}
+		data = append(data, temp)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
