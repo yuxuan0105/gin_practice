d260ea4a5a966e673d0095c77334cd48f3ee62a0
syuan <sv870105@gmail.com>
2020-03-12 14:22:13 +0800

    update initdb.sql

--- database/initdb.sql
+++ database/initdb.sql
@@ -1,24 +1,11 @@
---CREATE USER webapp WITH PASSWORD '23698741';
---CREATE USER test WITH PASSWORD 'test';
-
---CREATE DATABASE webdb;
---\c webdb
-
---for test
-CREATE SCHEMA IF NOT EXISTS AUTHORIZATION test;
-CREATE TABLE IF NOT EXISTS test.account (
-    user_id serial PRIMARY KEY,
-    email VARCHAR (355) UNIQUE NOT NULL,
-    password VARCHAR (50) NOT NULL,
-    nickname VARCHAR (50) UNIQUE NOT NULL,
-    created_on TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-ALTER TABLE test.account OWNER TO test;
+-- create a user 'webapp'
+-- create a database 'webdb' 
+-- import tables with commend psql -U postgres webdb -a -f initdb.sql
 
 --for webapp
 CREATE SCHEMA IF NOT EXISTS AUTHORIZATION webapp;
 CREATE TABLE IF NOT EXISTS webapp.account (
-    user_id serial PRIMARY KEY,
+    user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email VARCHAR (355) UNIQUE NOT NULL,
     password VARCHAR (50) NOT NULL,
     nickname VARCHAR (50) UNIQUE NOT NULL,
